8c93c6741e5b05099804ab50f4321305e2dd99df
Aaron Ross <superhawk610@gmail.com>
2021-10-16 00:27:47 -0700

    port gatsby starter to TS, remove unused plugins

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const Header = () => (
+  <header>
+    <h1 style={{ margin: 0 }}>
+      <Link to="/">x86reference</Link>
+    </h1>
+  </header>
+);
+
+export default Header;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Header from './Header';
+
+export interface Props {
+  children?: any;
+}
+
+const Layout = ({ children }: Props) => {
+  // const data = useStaticQuery(graphql`
+  //   query SiteTitleQuery {
+  //     site {
+  //       siteMetadata {
+  //         title
+  //       }
+  //     }
+  //   }
+  // `);
+
+  return (
+    <>
+      <Header />
+      <div>
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.com">Gatsby</a>
+        </footer>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
