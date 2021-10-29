4b2a893f7bd5d0308c711a6c9b3f89d3902049d4
Aaron Ross <superhawk610@gmail.com>
2021-10-29 01:32:00 -0700

    add logo & improved search prompt image

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
       resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
-          include: /assets/,
+          include: /assets\/.*\.svg$/,
         },
       },
     },
