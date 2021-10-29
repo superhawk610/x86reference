ffc2b868335efdfa5fc1cc2cb5df53c1445b6506
superhawk610 <superhawk610@gmail.com>
2021-10-29 09:01:14 +0000

    Deploying to gh-pages from @ superhawk610/x86reference@ef3e2b75b96c78143a9e1bf107c812a754febb5c 🚀

---
+++ component---src-pages-404-tsx-3b071ee7679cd1ba4e31.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkx86reference=self.webpackChunkx86reference||[]).push([[218],{9786:function(e,t,n){n.r(t);var u=n(7294),l=n(9090),r=n(1566);t.default=function(){return u.createElement(l.Z,null,u.createElement(r.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-3b071ee7679cd1ba4e31.js.map
